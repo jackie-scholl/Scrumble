06c05e99849be75ea2352e14135030b80bd2ea1e
Aditi Joshi <aditisjoshi@gmail.com>
2015-06-28 03:24:53 +0000

    fixing the manage tasks purple page

--- scrumble/static/jquery.js
+++ scrumble/static/jquery.js
@@ -11,4 +11,9 @@ $(document).ready(function() {
       $("#addnew").prepend("<li class='list-group-item'> objective: " + objective + "<div class='dropdown pull-right no-padding'><button class='btn btn-default dropdown-toggle' type='button' id='dropdownMenu1' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>Assign To<span class='caret'></span></button><ul class='dropdown-menu' aria-labelledby='dropdownMenu1'><li><a href='#'><input class='selectall' type='checkbox' /> Select All</a></li><li><a href='#'><input class='checkbox1' type='checkbox' /> Group 1</a></li><li><a href='#'><input class='checkbox1' type='checkbox' /> Group 2</a></li><li><a href='#'><input class='checkbox1' type='checkbox' /> Group 3</a></li><li><a href='#'><input class='checkbox1' type='checkbox' /> Group 4</a></li></ul></div><br> task: " + task + "</li>")
   });
   
+  $(".new-project-card-save").click(function () {
+      var task = document.getElementById("project-task-input").value;
+      $("#addnew").prepend("<li class='list-group-item'> task: " + task + "</li>")
+  });
+  
 });
