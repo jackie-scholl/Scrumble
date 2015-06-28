12a098c6ac73ceaf7f4f25969531a7ce5ad32c35
Aditi Joshi <aditisjoshi@gmail.com>
2015-06-28 03:49:10 +0000

    fixing controller file

--- scrumble/controller.py
+++ scrumble/controller.py
@@ -16,22 +16,6 @@ def teacherindex():
     """Index Controller"""
     return render_template('teacherindex.html')
 
-@app.route('/rebuild/<boardname>')
-def rebuild(boardname):
-    """Rebuild Controller"""
-    from trolly.client import Client
-    from trolly.exceptions import ResourceUnavailable
-    import uuid
-    api_key = "bf71d01b024c31a1c294b4755af55add"
-    token = "404f4361c2e9bf578e355862ffaf603226c9bddfde60b4c44f6a3e9ed7d917cd"
-    c = Client(api_key, token)
-    boards = c.get_boards()
-    board = [x for x in boards if x.name == boardname][0]
-    lists = board.get_lists()
-    backlog = lists[0]
-    groups = [[x.strip() for x in y.name.split(",")] for y in lists[1].get_cards()]
-<<<<<<< HEAD
-    return cgi.escape(str("backlog: " + str(backlog) + "<br />groups: " + str(groups)))
   
 @app.route('/managetasks')
 def managetasks():
@@ -54,7 +38,21 @@ def managegroups():
 def managestudents():
     """Index Controller"""
     return render_template('managestudents.html')
-=======
+
+@app.route('/rebuild/<boardname>')
+def rebuild(boardname):
+    """Rebuild Controller"""
+    from trolly.client import Client
+    from trolly.exceptions import ResourceUnavailable
+    import uuid
+    api_key = "bf71d01b024c31a1c294b4755af55add"
+    token = "404f4361c2e9bf578e355862ffaf603226c9bddfde60b4c44f6a3e9ed7d917cd"
+    c = Client(api_key, token)
+    boards = c.get_boards()
+    board = [x for x in boards if x.name == boardname][0]
+    lists = board.get_lists()
+    backlog = lists[0]
+    groups = [[x.strip() for x in y.name.split(",")] for y in lists[1].get_cards()]
     boards_to_delete = [x for x in boards if x.name.find(boardname) == 0 and len(x.name) > len(boardname)]
     [b.close_board(True) for b in boards_to_delete]
     out = []
@@ -66,6 +64,9 @@ def managestudents():
                 b.add_member(get_email(m), m)
             except ResourceUnavailable:
                 pass
+        l = b.add_list(query_params={"name": "backlog"})
+        for c in b.get_cards():
+            l.add_card(query_params={"
     return cgi.escape(str(out))
 
 def create_board(client, name):
@@ -80,11 +81,9 @@ def get_email(name):
     try:
         return map[name]
     except KeyError:
-        return "example@example.edu"
-    #l = name.split(" ")
-    #return "%s.%s@sample.edu" % (l[0], l[1])
->>>>>>> master
-
+        l = name.split(" ")
+        return "%s.%s@example.edu" % (l[0], l[1])
+        
 @app.errorhandler(404)
 def handle_error(e):
     return render_template('404.html')
